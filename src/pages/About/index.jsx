414b13d8c8c1a67575d7063a3b4a006dd8cfaaed
Wilmar Oliveira <wilmar_oliveira13@hotmail.com>
2023-05-01 12:53:05 -0300

    Styling about page

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -3,27 +3,45 @@ import { TableActivities } from '../../components/TableActivities'
 import { TableOtherActivities } from '../../components/TableOtherActivities'
 import mainActivities from '../../mocks/mainActivities.json'
 import otherActivities from '../../mocks/otherActivities.json'
+import { Caption, ContainerAbout, Description } from './styles'
 
 export const About = () => {
    const { id } = useParams()
 
    return (
-      <div>
+      <ContainerAbout>
          <h3>Centro Espírita Caminho da Luz</h3>
-         <h4>Atividades no centro</h4>
+         <Caption>Atividades no centro</Caption>
          <TableActivities rows={mainActivities[id] || []} />
-         <h4>Outras Atividades</h4>
+         <Caption>Outras Atividades</Caption>
          <TableOtherActivities rows={otherActivities[id] || []} />
-         <h4>História</h4>
-         <p>
+         <Caption>História</Caption>
+         <Description>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat. Duis aute irure dolor in
             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-         </p>
-      </div>
+            culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. <br />
+            Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+         </Description>
+         <Caption>Contato</Caption>
+         <span>Nome: (34) 0000-0000</span>
+      </ContainerAbout>
    )
 }
---
+++ src/pages/About/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const ContainerAbout = styled.div`
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: center;
+   padding: 30px;
+`
+export const Caption = styled.h4`
+   padding-top: 20px;
+   padding-bottom: 10px;
+`
+
+export const Description = styled.p`
+   text-align: justify;
+`
