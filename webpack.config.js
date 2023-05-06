12a27cc46ed9ffd68aeb03ee2e9acc09704544ec
Wilmar Oliveira <wilmar_oliveira13@hotmail.com>
2023-05-06 12:04:44 -0300

    Change folder build

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,35 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "build"),
-    publicPath: "/",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "public", "index.html"),
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "build"),
-    },
-    historyApiFallback: true,
-    port: 3000,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"],
+   entry: './src/index.js',
+   output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
+   },
+   plugins: [
+      new HtmlWebpackPlugin({
+         template: path.join(__dirname, 'public', 'index.html'),
+      }),
+   ],
+   devServer: {
+      static: {
+         directory: path.join(__dirname, 'dist'),
       },
-    ],
-  },
-  resolve: {
-    extensions: ["*", ".js", ".jsx"],
-  },
-};
+      historyApiFallback: true,
+      port: 3000,
+   },
+   module: {
+      rules: [
+         {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader'],
+         },
+      ],
+   },
+   resolve: {
+      extensions: ['*', '.js', '.jsx'],
+   },
+}
