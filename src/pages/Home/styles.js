0544c0611a6c56a44c64a1abebdb91732f58a8b6
Wilmar Oliveira <wilmar_oliveira13@hotmail.com>
2023-05-06 11:50:44 -0300

    Adjust grid-template-columns

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -3,6 +3,6 @@ import styled from 'styled-components'
 export const ContainerCard = styled.div`
    width: 100%;
    display: grid;
-   grid-template-columns: repeat(auto-fill, 290px);
+   grid-template-columns: repeat(auto-fill, 275px);
    justify-content: space-around;
 `
